3488a6ced8a09f801c2f37fb6a65913e7dcd4e32
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-11-08 12:38:33 -0800

    Add new translator module
    Introduced a new translator module utilizing langchain and OpenAI. Updated the .gitignore file to include .idea/ and removed .cache directory.

---
+++ translator/index.js
@@ -0,0 +1,21 @@
+﻿import dotenv from "dotenv";
+dotenv.config();
+
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const modelName = "gpt-4o-mini";
+
+const model = new ChatOpenAI({ model: modelName });
+
+const messages = [
+    new SystemMessage("Translate the following from English into Italian"),
+    new HumanMessage("hi!"),
+];
+
+const messageChunk = await model.invoke(messages);
+
+const parser = new StringOutputParser();
+const stringResult = await parser.invoke(messageChunk);
+console.log(stringResult);
