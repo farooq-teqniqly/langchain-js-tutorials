1e8194b334c6647d260b338ee932af82cf0de949
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-11-09 10:13:24 -0800

    First commit of chatbot.

---
+++ chatbot/index.js
@@ -0,0 +1,15 @@
+﻿import dotenv from "dotenv";
+dotenv.config();
+
+import { ChatOpenAI } from "@langchain/openai";
+
+const modelName = process.env.MODEL_NAME || "gpt-4o-mini";
+const model = new ChatOpenAI({ model: modelName });
+
+let result = await model.invoke([
+    {role: "user", content: "Hi, I'm Farooq."},
+    {role: "assistant", content: "Hello Farooq! How can I assist you today?"},
+    {role: "user", content: "What's my name?"}
+]);
+
+console.log(result);
