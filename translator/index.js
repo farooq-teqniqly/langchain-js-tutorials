390eaf232f3f967c2e956480346bcb11f183567f
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-11-09 09:46:41 -0800

    Add command line parameters for the english text and target language.

--- translator/index.js
+++ translator/index.js
@@ -1,17 +1,38 @@
 ﻿import dotenv from "dotenv";
 dotenv.config();
 
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { ChatOpenAI } from "@langchain/openai";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
-const modelName = "gpt-4o-mini";
+const allowedLanguages = ["French", "Italian", "German", "Spanish"];
 
+const argv = yargs(hideBin(process.argv)).option("language", {
+    alias: "l",
+    type: "string",
+    description: "Language to translate to",
+    demandOption: true,
+    choices: allowedLanguages
+})
+.option("english", {
+        alias: "e",
+        type: "string",
+        description: "English text to translate",
+        demandOption: true
+})
+.argv;
+
+const modelName = "gpt-4o-mini";
 const model = new ChatOpenAI({ model: modelName });
 
+const {english} = argv;
+const targetLanguage = argv.language;
+
 const messages = [
-    new SystemMessage("Translate the following from English into Italian"),
-    new HumanMessage("hi!"),
+    new SystemMessage(`Translate the following from English into ${targetLanguage}`),
+    new HumanMessage(english),
 ];
 
 const messageChunk = await model.invoke(messages);
